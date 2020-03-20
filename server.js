805006de3150513dfb1dcdc9b4bdf5b0a03b16e6
Felipe Carvalho <felipe.carvalho07@gmail.com>
2020-03-19 21:06:48 -0300

    Model with Mongo - installing mongoose - creating schema - creating model Product

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
+// starting app
 const app = express();
 
+// starting db
+mongoose.connect('mongodb://localhost:27017/nodeapi', {useNewUrlParser: true});
+requireDir('./src/models');
+
+// route
 app.get('/', (req, res) => {
-	res.send('Hello Rocketseat');
+	return res.send('Hello Rocketseat');
 });
 
 app.listen(3001);
